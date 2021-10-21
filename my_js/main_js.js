9f109b771dcc2f9c08a41534100c839896c856e8
unknown <you@example.com>
2021-10-21 22:04:54 +0700

    First plsssss

---
+++ my_js/main_js.js
@@ -0,0 +1,12 @@
+{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<SCRIPT LANGUAGE = "Javascript">
+	$(document).ready(function() {
+	$("a[href*='#']:not([href='#])").click(function() {
+		let target = $(this).attr("href");
+		$('html,body').stop().animate({
+		scrollTop: $(target).offset().top
+		}, 1000);
+		event.preventDefault();
+	});
+	});
+    </SCRIPT> */}
